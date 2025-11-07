983c09ff9769b027e3a2651728e852884ef9b379
Sebastian Ehlert <sehlert@microsoft.com>
2025-11-07 10:08:35 +0100

    Allow using PySCF 2.10 and later
    - When creating the density fitting object the xc method must be a
      known libxc functional to determine whether it is a hybrid.
    - Calling `predefined_auxbasis(..., xc="tpss")` before applying
      density fitting as this routine returns always None for non-hybrids
    - Fix this by temporarily setting `xc = "tpss"` before applying
      density fitting and revert to `xc = "custom"` to restore guard

--- tests/test_pyscf_classes.py
+++ tests/test_pyscf_classes.py
@@ -41,6 +41,7 @@ def test_skala_class(
         with_newton=with_newton,
         with_dftd3=with_dftd3,
     )
+    assert ks.xc == "custom"
     assert isinstance(ks, SkalaRKS if mol.spin == 0 else SkalaUKS)
     assert ks.with_dftd3 is not None if with_dftd3 else ks.with_dftd3 is None
 
