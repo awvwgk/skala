0ebc037f82cfed348b6ed48c1e10d5fd15db05d1
Sebastian Ehlert <sehlert@microsoft.com>
2026-02-02 11:13:02 +0100

    Update GauXC release tarball

--- examples/cpp/gauxc_integration/cmake/skala-dep-versions.cmake
+++ examples/cpp/gauxc_integration/cmake/skala-dep-versions.cmake
@@ -1,5 +1,5 @@
-set(Skala_GauXC_URL "https://github.com/microsoft/skala/releases/download/v1.1.0/gauxc-skala.tar.gz")
-set(Skala_GauXC_SHA256 "e0346c62453eef58ba3ee52c257370ee8abcbf00fbb1b4ea2e0bb879225e06be")
+set(Skala_GauXC_URL "https://github.com/microsoft/skala/releases/download/v1.1.1/gauxc-skala.tar.gz")
+set(Skala_GauXC_SHA256 "2b43e5708aeda962bb443838e34b08862ffa287a939b10ae80626811e057c44b")
 
 set(Skala_CLI11_URL "https://github.com/CLIUtils/CLI11/archive/refs/tags/v2.6.1.tar.gz")
 set(Skala_CLI11_SHA256 "377691f3fac2b340f12a2f79f523c780564578ba3d6eaf5238e9f35895d5ba95")
--- examples/fortran/gauxc_integration/cmake/skala-dep-versions.cmake
+++ examples/fortran/gauxc_integration/cmake/skala-dep-versions.cmake
@@ -1,5 +1,5 @@
 set(Skala_GauXC_URL "https://github.com/microsoft/skala/releases/download/v1.1.1/gauxc-skala.tar.gz")
-set(Skala_GauXC_SHA256 "93011cc5a7e9e1d3d065a4fd14deb2914274c93283a29676439e885003ac4436")
+set(Skala_GauXC_SHA256 "2b43e5708aeda962bb443838e34b08862ffa287a939b10ae80626811e057c44b")
 
 set(Skala_FLAP_URL "https://github.com/szaghi/FLAP/releases/download/v1.2.5/FLAP.tar.gz")
 set(Skala_FLAP_SHA256 "f2a388898a1ee49a8559822280647eb112d884484cb6169710011daab6dbb501")
