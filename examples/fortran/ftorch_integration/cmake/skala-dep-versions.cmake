70fb887a54d0297c145d6ef95f109f4ad3abc976
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2026-02-26 09:34:01 +0100

    Add example for running Skala directly in Fortran (#29)

---
+++ examples/fortran/ftorch_integration/cmake/skala-dep-versions.cmake
@@ -0,0 +1,2 @@
+set(Skala_FTorch_URL "https://github.com/Cambridge-ICCS/FTorch/archive/refs/tags/v1.0.0.tar.gz")
+set(Skala_FTorch_SHA256 "c4b6741e582623b7ecaecd59d02f779e8a6f6017f8068c85da8a034f468df375")
